b10773f762e195d8e39dc845cebb8d61f1653881
Valter <valter.nilo.jr@gmail.com>
2023-03-06 22:43:31 +0100

    Criação da estrutura dos arquivos e definição das classes

---
+++ src/ics_brasil.py
@@ -0,0 +1,7 @@
+# Import aux modules
+from Database.Database import Database
+from Graphics.Graphics import Graphics
+# Import python libraries
+import os
+# Define a relative path (current code)
+RELATIVE_PATH = os.path.dirname(os.path.abspath(__file__))
