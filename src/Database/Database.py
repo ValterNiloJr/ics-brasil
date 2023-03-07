137ec40a7a05e8d19db664b273fa83a36a4e779b
Valter <valter.nilo.jr@gmail.com>
2023-03-07 21:42:34 +0100

    Adição das extensões em Database

--- src/Database/Database.py
+++ src/Database/Database.py
@@ -1,19 +1,29 @@
+from typing import Literal
 # Import python libraries
 import pandas as pd
 import os
 
 class Database:
-    def __init__(self, path:str, extension='.xlsx') -> None:
+    def __init__(self, path:str, extension:Literal[".csv"] | None = '.xlsx') -> None:
         # local dict db
         self.db = {}
+
         # Construct local dict db walking in path
         for root, dirs, files in os.walk(path):
             for file in files:
-                # verify files with parameter extension
-                if file.endswith(extension):
+                # verify files with xlsx extension
+                if file.endswith(extension) and extension == '.xlsx':
                     # get the full path of the file
                     filepath = os.path.join(root, file)
                     df = pd.read_excel(filepath, dtype='str', engine='openpyxl')
+                    # Update local dict db with {key:'file_name'(without extension), value:'df'}
+                    self.db.update({file[:6]:df})
+
+                # verify files with csv extension
+                elif file.endswith(extension) and extension == '.csv':
+                    # get the full path of the file
+                    filepath = os.path.join(root, file)
+                    df = pd.read_csv(filepath, dtype='str', engine='openpyxl')
                     # Update local dict db with {key:'file_name'(without extension), value:'df'}
                     self.db.update({file[:6]:df})
 
@@ -27,4 +37,4 @@ if __name__ == '__main__':
     RELATIVE_PATH = os.path.dirname(os.path.abspath(__file__))
     DATABASE_PATH = os.path.abspath(os.path.join(RELATIVE_PATH, '../../datasets/brazil'))
 
-    new_db = Database(DATABASE_PATH).show()
+    new_db = Database(path=DATABASE_PATH).show()
