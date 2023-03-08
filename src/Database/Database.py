b6278e94534ff5d4dc57010c2ade08df16b2b5ad
Valter <valter.nilo.jr@gmail.com>
2023-03-08 12:10:12 +0100

    Implementação das funções de filtro

--- src/Database/Database.py
+++ src/Database/Database.py
@@ -4,10 +4,11 @@ import pandas as pd
 import os
 
 class Database:
-    def __init__(self, path:str, extension:Literal[".csv"] | None = '.xlsx') -> None:
+    def __init__(self) -> None:
         # local dict db
         self.db = {}
-
+    
+    def read_data(self, path:str, extension:Literal[".csv"] | None = '.xlsx'):
         # Construct local dict db walking in path
         for root, dirs, files in os.walk(path):
             for file in files:
@@ -26,15 +27,50 @@ class Database:
                     df = pd.read_csv(filepath, dtype='str', engine='openpyxl')
                     # Update local dict db with {key:'file_name'(without extension), value:'df'}
                     self.db.update({file[:6]:df})
+                
+                else:
+                    self.db = {}
 
-    def show(self):
-        print(self.db['Goal11'])
+        return self.db
 
+    def filter_all(self, remove_na=True, by_columns=[], **by_groups) -> dict:
+        new_db = {}
+        for data in self.db:
+            new_db.update({data : self.filter_data(self.db[data], remove_na, by_columns, **by_groups)})
+        return new_db
+
+    def filter_data(self, data:pd.DataFrame, remove_na=True, by_columns=[], **by_groups) -> pd.DataFrame:
+        # Copy from data to df
+        df = data.copy()
+        # filter by groups
+        # key = column name | value = value to filter
+        for column, values in zip(by_groups.keys(), by_groups.values()):
+            for value in values:
+                df = df.loc[(df[column]==value), by_columns]
+        # remove rows with all NaN data
+        if remove_na:
+            df.dropna(axis=1, how='all', inplace=True)
+
+        return df
+    
+#    def get_all(self) -> dict:
+#        return self.db
+#
+#    def get_data(self, key:str) -> pd.DataFrame:
+#        try:
+#            return self.db[key]
+#        except KeyError:
+#            raise KeyError(f'Key not found. Try: {list(self.db.keys())}')
 
 
 if __name__ == '__main__':
     # Define a relative path (current code)
     RELATIVE_PATH = os.path.dirname(os.path.abspath(__file__))
     DATABASE_PATH = os.path.abspath(os.path.join(RELATIVE_PATH, '../../datasets/brazil'))
-
-    new_db = Database(path=DATABASE_PATH).show()
+    columns = ['Goal', 'Indicator', 'SeriesCode', 'SeriesDescription',
+             'GeoAreaName', 'TimePeriod', 'Value', 'Sex', 'Units', 'Age']
+    db = Database()
+    data = db.read_data(path=DATABASE_PATH)
+    filtered = db.filter_all(by_columns=columns, GeoAreaName=['Brazil'])
+    #print(new_db.get_all())
+    #print(new_db.get_data('Goal03'))
